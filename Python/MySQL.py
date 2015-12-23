e0998b8ef8936bc20bbcb3e8c5be3b3b69fa0d1a
liyibocheng <yibocheng_li@loongjoy.com>
2015-12-23 20:01:58 +0800

    add find_all function

--- Python/MySQL.py
+++ Python/MySQL.py
@@ -6,7 +6,6 @@ import MySQLdb
 import time
 
 class MySQL:
-    error_code = ''
 
     _instance = None # 本类的实例
     _conn = None     # 数据库conn
@@ -14,8 +13,10 @@ class MySQL:
 
     _TIMEOUT = 30 #默认超时30秒
     _timecount = 0
+    log_file = None
 
     def __init__(self, dbconfig):
+        self.log_file = open('SQLException.log', 'w')
         try:
             self._conn = MySQLdb.connect(
                 host=dbconfig['host'],
@@ -26,15 +27,38 @@ class MySQL:
                 charset=dbconfig['charset']
             )
         except MySQLdb.Error, e:
-            self.error_code = e.args[0]
-            error_msg = 'MySQL ERROR: ', e.args[0], e.args[1]
+            error_msg = 'MySQL ERROR %d: %s' % (e.args[0], e.args[1])
+            print(error_msg)
+            self.log_file.write(error_msg)
+        self._cur = self._conn.cursor(MySQLdb.cursors.DictCursor)
 
-            if self._timecount < self._TIMEOUT:
-                interval = 5
-                self._timecount += interval
-                time.sleep(interval)
-                return self.__init__(dbconfig)
+
+    def find_all(self, table_name, condition='', params=list):
+        sql = "SELECT * FROM "
+        if table_name:
+            sql += table_name
+        if condition:
+            sql += ' WHERE ' + condition
+        try:
+            if params:
+                self._cur.execute(sql, params)
             else:
-                raise Exception(error_msg)
-        self._cur = self._conn.cursor()
-        self._instance = MySQLdb
+                self._cur.execute(sql)
+            return self._cur.fetchall()
+        except MySQLdb.Error, e:
+            error_msg = 'MySQL ERROR %d: %s' % (e.args[0], e.args[1])
+            print(error_msg)
+            self.log_file.write(error_msg)
+
+config = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'passwd': 'root',
+    'db': 'lenovo',
+    'charset': 'utf8'
+}
+db = MySQL(config)
+print(time.time())
+print db.find_all('Vendor',params=[])
+print(time.time())
