bb00e6e67b400c9c6b7ce9bf5cc7081b6c8b4d0b
LiYiBoCheng <yibocheng_li@loongjoy.com>
2016-01-08 14:13:29 +0800

    add class

--- Python/whatTimeOffWork.py
+++ Python/whatTimeOffWork.py
@@ -1,24 +1,42 @@
+# coding: utf-8
+
 import time
 from Tkinter import *
 import tkMessageBox
 
-hour = 0
-minute = 0
-second = 0
-top = Tk()
-var = StringVar()
-label = Label(top)
-label.pack()
-while True:
-    time.sleep(1)
-    second += 1
-    if second > 59:
-        second = 0
-        minute += 1
-    if minute > 59:
-        hour += 1
-    if minute == 1:
-        tkMessageBox.showinfo(title='after work', message='after work')
-    label['text'] = str(hour) + ':' + str(minute) + ':' + str(second)
-    top.update()
-top.mainloop()
+"""
+写这个脚本初衷是因为上班时间不固定,导致下班时候已经忘记了早上几点打卡
+如果忘记时间早走了会扣工资
+将脚本设置为开机自动运行,9小时候弹窗提示,顺便没过一个小时提醒保护颈椎
+"""
+class time_off:
+    hour = 0
+    minute = 0
+    second = 0
+
+    def __init__(self):
+        self.root = Tk()
+        self.label = Label(self.root)
+
+    def start(self):
+        self.label.pack()
+        for s in range(1, 86400):
+            time.sleep(1)
+            self.second += 1
+            if self.second > 59:
+                self.second = 0
+                self.minute += 1
+            if self.minute > 59:
+                self.minute = 0
+                self.hour += 1
+
+            if self.hour == 9 and self.minute == 0 and self.second == 0:
+                tkMessageBox.showinfo(title='yo~~~', message='! ! !')
+            if self.minute == 0 and self.second == 0:
+                tkMessageBox.showinfo(title='休息一下', message='动一动,喝口水')
+            self.label['text'] = str(self.hour) + ':' + str(self.minute) + ':' + str(self.second)
+            self.root.update()
+        self.root.mainloop()
+
+app = time_off()
+app.start()
