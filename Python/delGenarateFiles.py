f33d3a35811e1fa88191ba2f943be2d15cfcafe3
LiYiBoCheng <yibocheng_li@loongjoy.com>
2016-01-14 14:38:19 +0800

    change tab to space

--- Python/delGenarateFiles.py
+++ Python/delGenarateFiles.py
@@ -2,14 +2,14 @@ import os
 import time
 import shutil
 def is_date(str):
-	try:
-		time.strptime(str, "%Y-%m-%d")
-		return True
+    try:
+        time.strptime(str, "%Y-%m-%d")
+        return True
 	except:
-		return False
+        return False
 
 
 filepath = "D:\workspace\upupw\htdocs\LenovoCrm\uploads"
 for filename in os.listdir(filepath):
-	if is_date(filename):
-		shutil.rmtree(filepath + "\\" + filename)
+    if is_date(filename):
+        shutil.rmtree(filepath + "\\" + filename)
